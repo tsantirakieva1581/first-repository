410924fd7efe9f97540191b57dbdb3514b094e2d
tsantirakieva1581 <tsantiraki.evangelia@gmail.com>
2019-11-14 15:32:07 +0200

    edited something

--- Student.cs
+++ Student.cs
@@ -5,7 +5,7 @@ namespace New_Project{
         public int Id{get; set;}
         public string FirstName{get; set;}
         public string LastName {get; set;}
-        public DateTime BirthDa {get; set;}
+        public DateTime BirthDate {get; set;}
 
     
     }
