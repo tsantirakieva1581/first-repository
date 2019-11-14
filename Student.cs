ac1e09c08dc063e298ee8b5e2407f098f1c95544
tsantirakieva1581 <tsantiraki.evangelia@gmail.com>
2019-11-14 15:05:05 +0200

    create project

---
+++ Student.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace New_Project{
+    public class Student {
+        public int Id{get; set;}
+        public string FirstName{get; set;}
+        public string LastName {get; set;}
+    }
+}
