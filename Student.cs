8d7c61857380e5c7ab74c18a29971ff10ae829e1
tsantirakieva1581 <tsantiraki.evangelia@gmail.com>
2019-11-14 15:13:01 +0200

    Added Birthdate property

--- Student.cs
+++ Student.cs
@@ -5,5 +5,6 @@ namespace New_Project{
         public int Id{get; set;}
         public string FirstName{get; set;}
         public string LastName {get; set;}
+        public DateTime BirthDate {get; set;}
     }
 }
